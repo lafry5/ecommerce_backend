0a14c44b1a9f041cf484814a82cde81420fc5baa
lafry5 <lafry5@yahoo.com>
2020-08-20 18:41:24 -0700

    working on index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  foreignKey: ''
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ''
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'producttag_product',
+  foreignKey: ''
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'producttag_tag',
+  foreignKey: ''
+});
 
 module.exports = {
   Product,
